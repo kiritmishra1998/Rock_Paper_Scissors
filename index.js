90355f10e44aa08f701c4e41b019b6375811ee5d
Kirit Mishra <kiritmishra1998@gmail.com>
2026-09-16 22:19:44 +0530

    Add calculator project files

--- index.js
+++ index.js
@@ -1,48 +1,21 @@
 const display=document.getElementById("display");
-let timer=null;
-let startTime=0;
-let elapsedTime=0;
-let isRunning=false;
 
-function start(){
-if(!isRunning)
+function appendToDisplay(input)
 {
-    startTime=Date.now()-elapsedTime;
-    timer=setInterval(update,10);
-    isRunning=true;
-}
+display.value += input;
 }
 
-function stop(){
-if(isRunning)
+function clearDisplay()
 {
-    clearInterval(timer);
-    elapsedTime=Date.now-startTime;
-    isRunning=false;
-}
+display.value="";
 }
 
-function reset(){
-clearInterval(timer);
-startTime=0;
-elapsedTime=0;
-isRunning=false;
-display.textContent="00:00:00:00";
+function calculate()
+{
+    try{
+display.value=eval(display.value)
+    }
+catch(error){
+    display.value="Error";
 }
-
-function update(){
-const currentTime=Date.now();
-elapsedTime=currentTime-startTime;
-
-let hours = Math.floor(elapsedTime/(1000*60*60));
-let minutes =Math.floor(elapsedTime/(1000*60)%60);
-let seconds=Math.floor(elapsedTime/1000%60);
-let milliseconds=Math.floor(elapsedTime%1000 /10);
-
-hours=String(hours).padStart(2,"0");
-minutes=String(minutes).padStart(2,"0");
-seconds=String(seconds).padStart(2,"0");
-milliseconds=String(milliseconds).padStart(2,"0");
-
-display.textContent=`${hours}:${minutes}:${seconds}:${milliseconds}`;
 }
